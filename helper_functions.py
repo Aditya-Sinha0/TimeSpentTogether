9fcac25cbc7058310d8f980a514f5173b8ad6a5a
Aditya Sinha <sinhaditya02@gmail.com>
2023-06-20 14:23:03 -0700

    fix bugs

--- helper_functions.py
+++ helper_functions.py
@@ -11,8 +11,9 @@ def get_more_time_together_date(birthdate, connection_date):
 def percent_time_spent_together(birthday, connection_date):
     birthday = datetime.strptime(birthday, "%Y-%m-%d").date()
     connection_date = datetime.strptime(connection_date, "%Y-%m-%d").date()
-    total_days = (connection_date - birthday).days
-    time_together_days = (datetime.now().date() - connection_date).days
-    user_percent_time_spent_together = round((time_together_days / total_days) * 100, 2)
+
+    total_days_lived = (datetime.now().date() - birthday).days
+    total_days_together = (datetime.now().date() - connection_date).days
+    user_percent_time_spent_together = round((total_days_together / total_days_lived) * 100, 2)
 
     return user_percent_time_spent_together
